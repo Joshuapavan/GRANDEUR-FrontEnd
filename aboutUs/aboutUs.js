a8a10bb89afa3c2b1acba31fee3477b0caab375f
vinayjoshva <91328195+vinayjoshva@users.noreply.github.com>
2022-03-26 11:42:29 +0530

    Add files via upload

--- aboutUs/aboutUs.js
+++ aboutUs/aboutUs.js
@@ -1,3 +1,9 @@
 var popover = new bootstrap.Popover(document.querySelector('#contactus'), {
   trigger: 'focus'
 })
+
+const linkedIn = document.querySelector('#vinayLI');
+linkedIn.addEventListener('click', (e) => {
+  console.log(e);
+  window.open("https://www.linkedin.com/in/vinay-joshva15/");
+})
