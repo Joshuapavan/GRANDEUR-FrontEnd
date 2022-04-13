5cae08fc40d3b31ff5e7341b05e5754316e7eee2
vinayjoshva <joshvavinay@gmail.com>
2022-04-13 12:56:19 +0530

    added a login and sign up page

--- home/home.js
+++ home/home.js
@@ -1,3 +1,13 @@
 var popover = new bootstrap.Popover(document.querySelector('#contactus'), {
   trigger: 'focus'
 })
+
+// var popover = new bootstrap.Popover(document.querySelector('#account-click'), {
+//   trigger: 'focus'
+// })
+const account = document.querySelector('#account-click');
+
+account.addEventListener('click', () => {
+  // preventDefault();
+  window.location.href = '/Grandeur-FrontEnd/home/home.html';
+})
