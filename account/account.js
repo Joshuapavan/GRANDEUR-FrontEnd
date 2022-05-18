31801d709a5f4ba83cbb34e03ade97ca29c3ebfb
Joshuapavan <joshuapavan35@gmail.com>
2022-05-18 23:24:23 +0530

    Created sign in page frontend algorithm.

--- account/account.js
+++ account/account.js
@@ -1,6 +1,50 @@
-const account = document.querySelector('#account-click');
+// const account = document.querySelector('#account-click');
 
-account.addEventListener('click', () => {
-  // preventDefault();
-  window.location.href = '/Grandeur-FrontEnd/home/home.html';
-})
+// account.addEventListener('click', () => {
+//   // preventDefault();
+//   window.location.href = '/Grandeur-FrontEnd/home/home.html';
+// })
+
+
+
+async function validate() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const mailRegex = '[a-z0-9]+@[a-z]+\.[a-z]{2,3}';
+
+  if(!email.match(mailRegex)){
+    alert('Please enter a valid email address');
+  }
+  else
+  if(email.length > 0 && password.length > 0){
+    await fetch('http://localhost:8090/api/v1/clients/signIn',{
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body : JSON.stringify(
+      {
+        'email' : email,
+        'password' : password
+      }
+    )
+  })
+  .then(response => {
+    if(response.status >= 200 && response.status < 300) {
+      // printing something related to success response // 
+      alert("welcome to Grandeur")
+      // window.location.href = './/home/home.html';
+    }
+    
+    if(response.status >= 400 && response.status < 500) {
+      alert("Please enter a valid email address")
+    }
+    
+    if(response.status >= 500 && response.status < 600) {
+      alert(response.statusText)
+    }
+  })
+}
+else{
+  alert('Enter email and password');
+  }
+}
--- sign-up/signUp.js
+++ sign-up/signUp.js
@@ -1,10 +1,9 @@
-function validate() {
+async function validate() {
   const firstName=document.getElementById("firstName").value;
   const lastName=document.getElementById("lastName").value;
   const email=document.getElementById("email").value;
   const pwd=document.getElementById("pwd").value;
   const confirmPwd=document.getElementById("confirmPwd").value;
-  const buttonClick = document.getElementById("buttonClick");
   const errorMessage = document.getElementById("error-message");
   const fullName=firstName+lastName;
   const mailRegex = '[a-z0-9]+@[a-z]+\.[a-z]{2,3}';
@@ -12,32 +11,43 @@ function validate() {
 
   if(!email.match(mailRegex)) {
     labelForMail.innerHTML = "invalid email Id";
-  } 
-
+  }
+  else{
   if(pwd !== confirmPwd){
     errorMessage.innerHTML = "passwords don't match. Please try again!";
   }
-  fetch("http://localhost:8090/api/v1/registration", {
-       method: "POST",
-      body: JSON.stringify({
+  else{
+      await fetch("http://localhost:8090/api/v1/registration", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(
+        {
         "name" : fullName,
         "email" : email,
         "password": pwd
-    }),
-    headers: {
-        "Content-type": "application/json; charset=UTF-8"
-    }
-
+      })
+}).then(response => {
+  if(response.status >= 200 && response.status < 300) {
+    // printing something related to success response // 
+    alert("welcome "+fullName+" to Grandeur")
+  }
+  
+  if(response.status >= 400 && response.status < 500) {
+    alert("the email "+email+" already exists"+response.status)
+  }
+  
+  if(response.status >= 500 && response.status < 600) {
+    alert("Internal server error: "+response.status)
+  }
 })
 
-.then(response => response.json())
-.then(data => {
-  window.alert('Email already exist:', data);
-})
-.catch((error) => {
-  window.alert('Please check your mail to validate:', error);
-});
-
-
+// .then(data => {
+//   window.alert('Email already exist:', data.email);
+//   // console.log(data);
+// })
+// .catch((error) => {
+//   window.alert('Please check your mail to validate:', error);
+// });
+  }
+  }
 }
-
